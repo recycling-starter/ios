a847c13fdaae1b64b5a46002f0a2f70ba2982b6a
matvey <bme15122000@gmail.com>
2020-09-17 23:39:25 +0300

    style(register) add name to fields

--- RecyclingStarter/View/RegistrationViewController/RegisterViewController.swift
+++ RecyclingStarter/View/RegistrationViewController/RegisterViewController.swift
@@ -22,18 +22,20 @@ class RegisterViewController: UIViewController {
     private let codeField: UIUnderlinedTextField
     private let regLabel: UILabel
     private let regButton: UIButton
+    private let closeButton: UIButton
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        self.nameField = Self.makeField(placeholder: "name")
-        self.surnameField = Self.makeField(placeholder: "surname")
-        self.emailField = Self.makeField(placeholder: "email")
+        self.nameField = Self.makeField(placeholder: "Имя")
+        self.surnameField = Self.makeField(placeholder: "Фамилия")
+        self.emailField = Self.makeField(placeholder: "e-mail")
         self.phoneField = Self.makeField(placeholder: "phone")
-        self.passwordField = Self.makeField(placeholder: "password")
-        self.adressField = Self.makeField(placeholder: "adress")
-        self.officeField = Self.makeField(placeholder: "office")
-        self.codeField = Self.makeField(placeholder: "code")
-        self.regLabel = Self.makeLabel(text: "Registration")
-        self.regButton = Self.makeButton(text: "Do registration")
+        self.passwordField = Self.makeField(placeholder: "Пароль")
+        self.adressField = Self.makeField(placeholder: "Адресс здания")
+        self.officeField = Self.makeField(placeholder: "Номер офиса")
+        self.codeField = Self.makeField(placeholder: "Код регистрации")
+        self.regLabel = Self.makeLabel(text: "Регистрация")
+        self.regButton = Self.makeButton(text: "Зарегистрироваться")
+        self.closeButton = Self.makeCloseButton()
         
         super.init(nibName: nil, bundle: nil)
     }
@@ -45,15 +47,16 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpViews()
+        setupViews()
+        setupDelegate()
+        setupActions()
     }
     
-    func setUpViews() {
+    func setupViews() {
         view.backgroundColor = AppColor.authBackground
         
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.distribution = .fillEqually
         
         stackView.addArrangedSubview(nameField)
         stackView.addArrangedSubview(surnameField)
@@ -63,23 +66,62 @@ class RegisterViewController: UIViewController {
         stackView.addArrangedSubview(adressField)
         stackView.addArrangedSubview(officeField)
         stackView.addArrangedSubview(codeField)
-        stackView.addArrangedSubview(regButton)
         
         stackView.spacing = 15
         
-        view.addSubview(stackView)
-        stackView.topToSuperview(offset: 40)
-        stackView.bottomToSuperview(offset: -20)
-        stackView.leadingToSuperview(offset: 20)
-        stackView.trailingToSuperview(offset: 20)
+        view.addSubview(closeButton)
+        closeButton.topToSuperview(offset: 36, usingSafeArea: true)
+        closeButton.leadingToSuperview(offset: 20)
+        closeButton.width(40)
+        closeButton.height(40)
+        
+        view.addSubview(regLabel)
+//        regLabel.topToSuperview(offset: 36)
+//        regLabel.leadingToSuperview(offset: 46)
+        regLabel.centerY(to: closeButton)
+        regLabel.leadingToTrailing(of: closeButton, offset: 5)
+        
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        
+        view.addSubview(scrollView)
+        scrollView.topToBottom(of: regLabel, offset: 20)
+        scrollView.leadingToSuperview(offset: 30)
+        scrollView.trailingToSuperview(offset: 30)
+        
+        view.addSubview(regButton)
+        regButton.width(to: scrollView)
+        regButton.centerXToSuperview()
+        regButton.topToBottom(of: scrollView, offset: 10)
+        regButton.bottomToSuperview(offset: -20, usingSafeArea: true)
+        
+        scrollView.addSubview(stackView)
+        
+        stackView.topToSuperview()
+        stackView.bottomToSuperview()
+        stackView.leadingToSuperview()
+        stackView.width(to: scrollView)
     }
     
+    private func setupDelegate() {
+        nameField.delegate = self
+        surnameField.delegate = self
+        emailField.delegate = self
+        phoneField.delegate = self
+        passwordField.delegate = self
+        adressField.delegate = self
+        officeField.delegate = self
+        codeField.delegate = self
+    }
+    
+    //MARK: Make UI elemets
     private static func makeField(placeholder: String) -> UIUnderlinedTextField {
         let field = UIUnderlinedTextField()
         field.tintColor = AppColor.underline
         field.attributedPlaceholder = placeholder.set(style: Style.placeholderStyle)
         field.textColor = Style.textFieldStyle.color?.color
         field.font = Style.textFieldStyle.font?.font(size: Style.textFieldStyle.size)
+        field.height(35)
         return field
     }
     
@@ -87,6 +129,7 @@ class RegisterViewController: UIViewController {
         let label = UILabel()
         label.textColor = Style.labelStyle.color?.color
         label.font = Style.labelStyle.font?.font(size: Style.labelStyle.size)
+        label.attributedText = text.set(style: Style.labelStyle)
         return label
     }
     
@@ -96,6 +139,17 @@ class RegisterViewController: UIViewController {
         button.setAttributedTitle(styledText, for: .normal)
         button.backgroundColor = AppColor.button
         button.layer.cornerRadius = 8
+        button.height(51)
+        return button
+    }
+    
+    private static func makeCloseButton() -> UIButton {
+        let button = UIButton()
+        let image = AppImage.logoutImage?.withRenderingMode(.alwaysTemplate)
+        button.setImage(image, for: .normal)
+        button.imageView?.tintColor = AppColor.label
+        button.imageView?.transform = CGAffineTransform(rotationAngle: .pi)
+        button.imageView?.height(28)
         return button
     }
     
@@ -122,4 +176,24 @@ class RegisterViewController: UIViewController {
         }
     }
     
+    //MARK: Actions
+    private func setupActions() {
+        closeButton.addTarget(self, action: #selector(closeView), for: .touchUpInside)
+    }
+    
+    @objc private func closeView() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+//MARK: UITextFieldDelegate
+extension RegisterViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
